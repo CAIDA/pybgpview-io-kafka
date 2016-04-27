fc45be46e4df1e3bed810646be01b48bf22a45a2
Alistair King <alistair@caida.org>
2016-04-27 16:30:18 +0000

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 #
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pybgpview',
+setup(name='pybgpview.io.kafka',
       version='0.1',
       description='BGPView Kafka IO module',
-      url='https://github.com/CAIDA/bgpview-io-kafka',
+      url='https://github.com/CAIDA/pybgpview-io-kafka',
       author='Alistair King',
       author_email='alistair@caida.org',
       license='BSD',
-      packages=['pybgpview'],
+      packages=find_packages(),
       entry_points={'console_scripts': [
           'bgpview-server-kafka=pybgpview.io.kafka.server:main'
       ]},
